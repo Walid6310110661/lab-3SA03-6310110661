4a827611b5f8dc151701e3983454391043d48022
Walid Hengbaru <6310110661@psu.ac.th>
2022-08-20 11:01:19 +0700

    part 7.3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import WordCard from './WordCard';
 const word1 = "Hello";
 const word2 = "World";
 const word3 = "Computer";
-
+const word4 = "Engineering";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <WordCard value={word1}/>
         <WordCard value={word2}/>
         <WordCard value={word3}/>
-
+        <WordCard value={word4}/>
     </div>
   );
 }
